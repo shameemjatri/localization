bdd9f59d9a126445106409203501cc26d81445ca
shameemjatri <shameem.jatri@gmail.com>
2020-08-23 16:10:26 +0600

    added localization

---
+++ app/src/main/java/co/jatri/localization/LocalizApplication.kt
@@ -0,0 +1,10 @@
+package co.jatri.localization
+
+import android.app.Application
+
+class LocalizApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
--- app/src/main/java/co/jatri/localization/MainActivity.kt
+++ app/src/main/java/co/jatri/localization/MainActivity.kt
@@ -2,44 +2,78 @@ package co.jatri.localization
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-
     private val sharedPrefFile = "kotlinsharedpreference"
-    val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
-    val editor:SharedPreferences.Editor =  sharedPreferences.edit()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val btnView = findViewById<Button>(R.id.button_language_switch)
+        val textView = findViewById<TextView>(R.id.text_view_hello)
+        textView.text=this.resources.getString(R.string.hello)
+
 
         btnView.setOnClickListener(View.OnClickListener {
-            val lang = sharedPreferences.getString("SELECTED_LANGUAGE", "en")
-            if (lang == "en"){
-                ApplicationLanguageHelper.wrap(this, lang!!)
-                editor.putString("SELECTED_LANGUAGE","en")
+            val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
+            val editor:SharedPreferences.Editor =  sharedPreferences.edit()
 
+            val language = sharedPreferences.getString("language","en")
+            if(language=="en"){
+                val localeToSwitchTo = Locale("en")
+                ContextUtils.updateLocale(this, localeToSwitchTo)
+                editor.putString("language","en")
             }else{
-                ApplicationLanguageHelper.wrap(this, lang!!)
-                editor.putString("SELECTED_LANGUAGE","bn")
+                val localeToSwitchTo = Locale("bn")
+                ContextUtils.updateLocale(this, localeToSwitchTo)
+                editor.putString("language","bn")
             }
+
 
             editor.apply()
             editor.commit()
 
+            textView.text=this.resources.getString(R.string.hello)
+            btnView.text=this.resources.getString(R.string.switch_language)
         })
     }
 
 
+
     override fun attachBaseContext(newBase: Context?) {
-        val lang = sharedPreferences.getString("SELECTED_LANGUAGE", "en")
-        super.attachBaseContext(ApplicationLanguageHelper.wrap(newBase!!, lang!!))
+        try {
+            val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
+            val language = sharedPreferences.getString("language","en")
+            if(language=="en"){
+                val localeToSwitchTo = Locale("en")
+                val localeUpdatedContext =
+                    ContextUtils.updateLocale(newBase!!, localeToSwitchTo)
+                super.attachBaseContext(localeUpdatedContext)
+            }else{
+                val localeToSwitchTo = Locale("bn")
+                val localeUpdatedContext =
+                    ContextUtils.updateLocale(newBase!!, localeToSwitchTo)
+                super.attachBaseContext(localeUpdatedContext)
+            }
+        }catch (e: Exception){
+            val localeToSwitchTo = Locale("en")
+            val localeUpdatedContext =
+                ContextUtils.updateLocale(newBase!!, localeToSwitchTo)
+            super.attachBaseContext(localeUpdatedContext)
+        }
+
+
+
     }
+
 
 }
